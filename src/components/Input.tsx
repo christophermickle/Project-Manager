c1c2a0b01e0531bb4294e64981168b0e20912052
Christopher Mickle <cmickle.dev@gmail.com>
2023-11-26 00:10:42 -0600

    Refactor Input component and add refs to NewProject component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { ForwardedRef, forwardRef } from "react";
 
 interface InputProps {
   label?: string;
@@ -6,39 +6,35 @@ interface InputProps {
   props?: React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>;
 }
 
-const Input: React.FC<InputProps> = ({ label, textarea, props }) => {
-  const classes =
-    "w-full p-1 border-b-2 rounded-sm border-slate-300 bg-slate-200 text-slate-600 focus:outline-none focus:border-slate-600";
+const Input = forwardRef(
+  (
+    { label, textarea, props }: InputProps,
+    ref: ForwardedRef<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const classes =
+      "w-full p-1 border-b-2 rounded-sm border-slate-300 bg-slate-200 text-slate-600 focus:outline-none focus:border-slate-600";
 
-  return (
-    <p className='flex flex-col gap-1 my-4'>
-      <label className='text-sm font-bold uppercase text-slate-500'>
-        {label}
-      </label>
-      {textarea ? (
-        <textarea className={classes} {...props} />
-      ) : (
-        <input className={classes} {...props} />
-      )}
-    </p>
-  );
-}
+    return (
+      <p className='flex flex-col gap-1 my-4'>
+        <label className='text-sm font-bold uppercase text-slate-500'>
+          {label}
+        </label>
+        {textarea ? (
+          <textarea
+            ref={ref as ForwardedRef<HTMLTextAreaElement>}
+            className={classes}
+            {...props}
+          />
+        ) : (
+          <input
+            ref={ref as ForwardedRef<HTMLInputElement>}
+            className={classes}
+            {...props}
+          />
+        )}
+      </p>
+    );
+  }
+);
 
 export default Input;
-
-// function Input({ label, textarea, ...props }: InputProps) {
-//   return (
-//     <p className='flex flex-col gap-1 my-4'>
-//       <label className='text-sm font-bold uppercase text-slate-500'>
-//         {label}
-//       </label>
-//       {textarea ? (
-//         <textarea className={classes} {...props} />
-//       ) : (
-//         <input className={classes} {...props} />
-//       )}
-//     </p>
-//   );
-// }
-
-// export default Input;
--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -1,6 +1,12 @@
+import { useRef } from "react";
 import Input from "./Input";
 
 function NewProject() {
+  // useRef<HTMLInputElement | HTMLTextAreaElement>(null) allows the ref to be an input or a textarea
+  const title = useRef<HTMLInputElement | HTMLTextAreaElement>(null);
+  const description = useRef<HTMLInputElement | HTMLTextAreaElement>(null);
+  const dueDate = useRef<HTMLInputElement | HTMLTextAreaElement>(null);
+
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
@@ -10,15 +16,15 @@ function NewProject() {
           </button>
         </li>
         <li>
-          <button className='bg-slate-800 text-slate-50 hover:bg-slate-950 px-6 py-2 rounded-md'>
+          <button className='px-6 py-2 rounded-md bg-slate-800 text-slate-50 hover:bg-slate-950'>
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label='Title' />
-        <Input label='Description' textarea />
-        <Input label='Due Date' />
+        <Input ref={title} label='Title' />
+        <Input ref={description} label='Description' textarea />
+        <Input ref={dueDate} label='Due Date' />
       </div>
     </div>
   );
